a2d7ccff649666736ba3b6c9ba298277610e3e54
denis <denis.s.sokolov@gmail.com>
2025-01-16 15:19:52 +0300

    rename web page tab

--- include/index.h
+++ include/index.h
@@ -4,7 +4,7 @@ const char index_html_template[] PROGMEM = R"rawhtml(
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=2.5, user-scalable=1">
-    <title>Luc's Linechart demo</title>
+    <title>Temperature Forecast Linechart demo</title>
     <style>
         body {
             background-color: powderblue;
