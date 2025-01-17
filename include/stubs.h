91e0a6cc0d628a7ad14a850714df10d388af81f2
denis <denis.s.sokolov@gmail.com>
2025-01-17 14:27:50 +0300

    move dummy casses to a new header

---
+++ include/stubs.h
@@ -0,0 +1,29 @@
+// #include <OneWire.h>
+// #include <DallasTemperature.h>
+
+// ---------------------------------------------------
+// dummy stub
+// ---------------------------------------------------
+#define ONE_WIRE_BUS 23
+
+class Dummy_OneWire
+{
+public:
+  Dummy_OneWire(int _tmp) {}
+};
+
+class Dummy_DallasTemperature
+{
+private:
+  mutable unsigned int m_value{0};
+
+public:
+  Dummy_DallasTemperature(const Dummy_OneWire *const _tmp) {}
+  void requestTemperatures() const {}
+  int getTempCByIndex(uint _aIndex) const
+  {
+    m_value = std::min(m_value + 5, 100U);
+    return m_value;
+  }
+  void begin() {}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -11,39 +11,7 @@
 
 #include "wifisecrets.h"
 #include "index.h"
-
-constexpr int listenPort{80};
-AsyncWebServer server(listenPort);
-
-// #include <OneWire.h>
-// #include <DallasTemperature.h>
-
-// ---------------------------------------------------
-// dummy stub
-// ---------------------------------------------------
-#define ONE_WIRE_BUS 23
-
-class Dummy_OneWire
-{
-public:
-  Dummy_OneWire(int _tmp) {}
-};
-
-class Dummy_DallasTemperature
-{
-private:
-  mutable unsigned int m_value{0};
-
-public:
-  Dummy_DallasTemperature(const Dummy_OneWire *const _tmp) {}
-  void requestTemperatures() const {}
-  int getTempCByIndex(uint _aIndex) const
-  {
-    m_value = std::min(m_value + 5, 100U);
-    return m_value;
-  }
-  void begin() {}
-};
+#include "stubs.h"
 
 // Setup a oneWire instance
 // OneWire oneWire(ONE_WIRE_BUS);
@@ -53,6 +21,9 @@ Dummy_OneWire oneWire(ONE_WIRE_BUS);
 // DallasTemperature sensors(&oneWire);
 Dummy_DallasTemperature sensors(&oneWire);
 // ---------------------------------------------------
+
+constexpr int listenPort{80};
+AsyncWebServer server(listenPort);
 
 constexpr size_t arraySize{10};
 int tempArray[arraySize] = {};
