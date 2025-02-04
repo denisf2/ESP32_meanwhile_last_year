c5c9a63600f5f5cb0a94a45e77fef709b8531662
denis <denis.s.sokolov@gmail.com>
2025-02-04 18:19:44 +0300

    add:deserialize JSON

--- include/IpGeolocationApi.h
+++ include/IpGeolocationApi.h
@@ -3,6 +3,12 @@
 
 #include <Arduino.h>
 
-auto GetLocationCoordinates(const String& aApiKey) -> void;
+auto GetLocationCoordinates(const String &aApiKey) -> void;
+struct Coordinates
+{
+    double latitude{0.0};
+    double longitude{0.0};
+};
 
+extern Coordinates Coordinates_;
 #endif /* IPGEOLOCATIONAPI_H__ */
--- src/IpGeolocationApi.cpp
+++ src/IpGeolocationApi.cpp
@@ -1,40 +1,74 @@
 #include "IpGeolocationApi.h"
 
 #include <HTTPClient.h>
+#include <ArduinoJson.h>
 #include <TLog.h>
 
-auto GetLocationCoordinates(const String& aApiKey) -> void {// [ ]FIXME:rename
-  const String serverName = "https://api.ipgeolocation.io/ipgeo";
+Coordinates Coordinates_;
 
-  // const String serverPath = serverName + "?apiKey={api_key}";
-  const String serverPath = serverName + "?apiKey=" + aApiKey;
+/*
+api description: https://ipgeolocation.io/ip-location-api.html#documentation-overview
+*/
 
-  TLog::println(serverPath);
+auto GetLocationCoordinates(const String &aApiKey) -> void
+{ // [ ]FIXME:rename
+    const String serverName = "https://api.ipgeolocation.io/ipgeo";
 
-  HTTPClient http;
-  // Your Domain name with URL path or IP address with path
-  http.begin(serverPath.c_str());
+    const String serverPath = serverName + "?apiKey=" + aApiKey;
 
-  // [ ]FIXME:remove
-  // If you need Node-RED/server authentication, insert user and password below
-  //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+    TLog::println(serverPath);
 
-  // Send HTTP GET request
-  int httpResponseCode = http.GET();
+    HTTPClient http;
+    // Your Domain name with URL path or IP address with path
+    http.begin(serverPath.c_str());
 
-  if (httpResponseCode > 0) 
-  {
-    // [ ]TODO: handle response codes and JSONs 200 401 404
-    TLog::println("HTTP Response code: ");
-    TLog::print(httpResponseCode);
-    const String payload = http.getString();
-    TLog::println(payload);
-  } 
-  else 
-  {
-    TLog::println("Error code: ");
-    TLog::print(httpResponseCode);
-  }
-  // Free resources
-  http.end();
+    // [ ]FIXME:remove
+    // If you need Node-RED/server authentication, insert user and password below
+    // http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+
+    // Send HTTP GET request
+    int httpResponseCode = http.GET();
+
+    if (httpResponseCode > 0)
+    {
+        // [ ]TODO: handle response codes and JSONs 200 401 404
+        TLog::println("HTTP Response code: ");
+        TLog::print(httpResponseCode);
+        const String payload = http.getString();
+        // TLog::println(payload);
+
+        // Allocate the JSON document
+        JsonDocument doc;
+        // Deserialize the JSON document
+        DeserializationError error = deserializeJson(doc, payload);
+
+        // Test if parsing succeeds.
+        if (error)
+        {
+            Serial.print(F("deserializeJson() failed: "));
+            Serial.println(error.f_str());
+            return;
+        }
+
+        // Fetch values.
+        //
+        // Most of the time, you can rely on the implicit casts.
+        // In other case, you can do doc["time"].as<long>();
+        Coordinates_.latitude = doc["latitude"];
+        Coordinates_.longitude = doc["longitude"];
+
+        // Print values.
+        TLog::println("Acquired coordinates: [");
+        TLog::print(Coordinates_.latitude, 6);
+        TLog::print(", ");
+        TLog::print(Coordinates_.longitude, 6);
+        TLog::print("]");
+    }
+    else
+    {
+        TLog::println("Error code: ");
+        TLog::print(httpResponseCode);
+    }
+    // Free resources
+    http.end();
 }
