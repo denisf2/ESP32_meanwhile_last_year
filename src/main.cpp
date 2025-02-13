8329cbcf41670e14369ba5496abbb09c85546087
denis <denis.s.sokolov@gmail.com>
2025-02-13 23:32:41 +0300

    add:extract TODOs

--- src/main.cpp
+++ src/main.cpp
@@ -246,25 +246,3 @@ get forcast by coordinate   |
     v                       |
 print city/forecast info <--|
 *************************/
-
-/* Roadmap:
-
-[x]TODO: get gps coordinates by ip
-[x]TODO: get forecast by gps coordinates
-[x]TODO: update wifi params from webpage
-[x]TODO: update api keys params from webpage
-[ ]TODO: visualizate forecast using google charts
-[x]TODO: export to a new module RestoreStoredData
-[ ]TODO: test connection and report before save them. Possible to lose wifi connection
-[x]TODO: prebuild acton to gzip index.html
-[ ]TODO: remove prebuild action from upload target
-[x]TODO: soft restart button on web page
-[ ]TODO: add status of coordinates acquisition
-[ ]TODO: add status of forecast acquisition
-[ ]TODO: in wifi AP mode web page freezes couse google charts js
-[x]TODO: set default SSID and pass in case "no connection to router". now it is last
-[x]TODO: setup and run default wifi access point on cold start
-[ ]TODO: store coordinates
-[ ]TODO: update coordinates button on web page
-
-*/
