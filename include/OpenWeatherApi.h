003f605a9b86e722d2d6fa485ce9a617ad990e6b
denis <denis.s.sokolov@gmail.com>
2025-03-21 22:19:21 +0300

    add:refactoring

--- include/OpenWeatherApi.h
+++ include/OpenWeatherApi.h
@@ -3,6 +3,12 @@
 
 #include <Arduino.h>
 
+struct Weather_t {
+    double temp{0.0};
+};
+
 auto GetForecast(const String &aApiKey, const String &aLat, const String &aLon) -> bool;
+
+extern Weather_t weather;
 
 #endif /* OPENWEATHERAPI_H__ */
--- src/OpenWeatherApi.cpp
+++ src/OpenWeatherApi.cpp
@@ -2,6 +2,7 @@
 
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
+#include <optional>
 
 /*
 api description: https://openweathermap.org/current
@@ -23,79 +24,89 @@ const String GEO_REVERSE_API = "https://api.openweathermap.org/geo/1.0/reverse";
 ```
 */
 
+Weather_t weather;
+
+auto ParseJsonOwtr(const String& aData) -> std::optional<Weather_t>
+{
+    // Allocate the JSON document
+    JsonDocument doc;
+    // Deserialize the JSON document
+    DeserializationError error = deserializeJson(doc, aData);
+
+    // Test if parsing succeeds.
+    if (error)
+    {
+        log_w("JSON deserialition failed. Error code: %s", error.c_str());
+        return std::nullopt;
+    }
+
+    if(!doc["cod"].is<int>())
+        return std::nullopt;
+
+    constexpr int respondOK{200};
+    constexpr int respondGoodKeyInvalidData{400};
+
+    const int code = doc["cod"].as<int>();
+
+    if (!(respondOK == code || respondGoodKeyInvalidData == code))
+        return std::nullopt;
+
+    // Fetch values.
+    Weather_t w;
+    w.temp = doc["main"]["temp"];
+
+    return std::make_optional(std::move(w));
+}
+
+auto GetApiUrl(const String &aApiKey, const String &aLat, const String &aLon)-> String
+{
+    const String units{"metric"};
+    return String("https://api.openweathermap.org/data/2.5/weather")
+                + "?lat=" + aLat
+                + "&lon=" + aLon
+                + "&appid=" + aApiKey
+                + "&units=" + units;
+}
+
 auto GetForecast(const String &aApiKey, const String &aLat, const String &aLon) -> bool // [ ]FIXME:rename
 {
-    const String serverName = "https://api.openweathermap.org/data/2.5/weather";
-    const String units{"metric"};
-    const String serverPath = serverName + "?lat=" + aLat + "&lon=" + aLon + "&appid=" + aApiKey + "&units=" + units;
-
+    const String serverPath = GetApiUrl(aLat, aLon, aApiKey);
     log_d("%s", serverPath.c_str());
 
     HTTPClient http;
     // Your Domain name with URL path or IP address with path
     http.begin(serverPath.c_str());
 
-    // [ ]FIXME:remove
-    // If you need Node-RED/server authentication, insert user and password below
+    // If you need server authentication, insert user and password below
     // http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
 
     // Send HTTP GET request
     int httpResponseCode = http.GET();
 
-    if (httpResponseCode > 0)
-    {
-        // [ ]TODO: handle response codes and JSONs 200 400 401 404
-        log_d("HTTP Response code: %d", httpResponseCode);
-        const String payload = http.getString();
-        // log_d("%s", payload.c_str());
-
-        // Allocate the JSON document
-        JsonDocument doc;
-        // Deserialize the JSON document
-        DeserializationError error = deserializeJson(doc, payload);
-
-        // Test if parsing succeeds.
-        if (error)
-        {
-            log_w("JSON deserialition failed. Error code: %s", error.c_str());
-            http.end();
-            return false;
-        }
-
-        if(doc["cod"].is<int>())
-        {
-            constexpr int respondOK{200};
-            constexpr int respondGoodKeyInvalidData{400};
-            const int code = doc["cod"].as<int>();
-            if (!(respondOK == code || respondGoodKeyInvalidData == code)){
-                http.end();
-                return false;
-            }
-        }
-        else
-        {
-            http.end();
-            return false;
-        }
-
-        // Fetch values.
-        //
-        // Most of the time, you can rely on the implicit casts.
-        // In other case, you can do doc["time"].as<long>();
-        double temp = doc["main"]["temp"];
-        // Coordinates_.longitude = doc["longitude"];
-
-        // // Print values.
-        log_d("Acquired temperature: [ %4.1f ]", temp);
-    }
-    else
+    if (httpResponseCode <= 0)
     {
         log_d("Error code: %d", httpResponseCode);
         http.end();
         return false;
     }
+
+    // [ ]TODO: handle response codes and JSONs 200 400 401 404
+    log_d("HTTP Response code: %d", httpResponseCode);
+    const String payload = http.getString();
+    // log_d("%s", payload.c_str());
+
+    auto res = ParseJsonOwtr(payload);
+
     // Free resources
     http.end();
+
+    if(!res)
+        return false;
+
+    weather = res.value();
+
+    // Print values.
+    log_d("Acquired temperature: [ %4.1f ]", weather.temp);
 
     return true;
 }
