9832bff4c4d621e438618e5bfb96f39d43978829
denis <denis.s.sokolov@gmail.com>
2025-05-23 23:35:58 +0300

    add:TODO

--- src/main.cpp
+++ src/main.cpp
@@ -121,9 +121,12 @@ auto job_acquire_coordinates(unsigned long aCurrent) -> void
         {
             // case automatic
             GetLocationCoordinates(GetIpGeoKey());
-
-            SaveLatitude(std::to_string(coordinates.latitude).c_str());
-            SaveLongitude(std::to_string(coordinates.longitude).c_str());
+            // [ ]TODO: save result on positive request status
+            // if(request is OK)
+            {
+                SaveLatitude(std::to_string(coordinates.latitude).c_str());
+                SaveLongitude(std::to_string(coordinates.longitude).c_str());
+            }
         }
         else
         {
